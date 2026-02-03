4d7baaa05df5a45194129ed1d5042358da2998bf
kol-kidd <uyancole@gmail.com>
2026-02-03 19:56:24 +0800

    chore: Remove Next.js todo-list project files

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">KoruBot TodoList</h1>
-      <p>Your minimalist to-do list powered by KoruBot.</p>
-    </main>
-  );
-}
